8fc343164fdd19a05b787d084b79b419fe9ec408
苏沐阳 <ucaime@users.noreply.github.com>
2020-12-09 17:01:37 +0800

    Create h3bpmlimitremover.js

---
+++ h3bpmlimitremover.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         氚云试用移除
+// @namespace    ucaime
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.h3yun.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle(".mainwrappermask { filter:none !important; }.limitmodal-mask{display:none !important;}");
+})();
